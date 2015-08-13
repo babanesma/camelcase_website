ae0321bc4f018260f24f9f9f7875309418b85a09
Adham Saad <adham.sa3d@gmail.com>
2015-08-13 12:24:57 +0200

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 
 /usr/local/bin/drush sql-drop --yes;
 latestDbFile=$(ls ./sql -t1 | grep wego | head -n 1);
-/usrl/local/bin/drush sql-cli < ./sql/$latestDbFile ;
+/usr/local/bin/drush sql-cli < ./sql/$latestDbFile ;
 /usr/local/bin/drush cache-clear all ;
