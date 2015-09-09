ad33df8aaa4a49c2da9e157b756e814f9df04b45
Adham Saad <adham.sa3d@gmail.com>
2015-09-09 16:21:10 +0200

    fixing deploy.sh to grab the latest db file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 
 composer install;
 ./vendor/bin/drush sql-drop --yes;
-latestDbFile=$(ls ./sql -t1 | grep wego | head -n 1);
-./vendor/bin/drush sql-cli < ./sql/$latestDbFile ;
+latestDbFile=$(ls -r -1 ./sql/wego_*.sql | head -n 1);
+./vendor/bin/drush sql-cli < $latestDbFile ;
 ./vendor/bin/drush cache-clear all ;
